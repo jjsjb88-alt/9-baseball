51a81fb587e3666c765c9df94094f91db9297586
jjsjb88-alt <jjsjb88@gmail.com>
2026-09-18 00:24:55 +0900

    feat(v10): load the STS landscape battleboard last

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./duel/landscape-first.css";
 import "./duel/landscape-scroll-fix.css";
 import "./duel/character-master.css";
 import "./duel/responsive-master.css";
+import "./duel/sts-battleboard.js";
+import "./duel/sts-battleboard.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
