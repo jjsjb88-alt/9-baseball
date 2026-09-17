ab56b270dc9df5160b5de9f5ffda0acfb9796160
jjsjb88-alt <jjsjb88@gmail.com>
2026-09-17 16:59:17 +0900

    feat(v10): load landscape-first mobile layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Duel from "./duel/App.jsx";
 import "./duel/stack-direct-tap.js";
+import "./duel/landscape-first.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
