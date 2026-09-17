4d6219057db1f0eb0209e3783fb9abb67859511a
jjsjb88-alt <jjsjb88@gmail.com>
2026-09-17 17:17:18 +0900

    fix(v10): load landscape scroll hotfix after canvas layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import Duel from "./duel/App.jsx";
 import "./duel/stack-direct-tap.js";
 import "./duel/landscape-first.css";
+import "./duel/landscape-scroll-fix.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
